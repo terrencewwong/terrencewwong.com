049dd41109de37f78d27d503d754c930e78d4c38
Terrence Wong <wongterrencew@gmail.com>
2019-04-29 18:39:46 +0200

    design: make content vertically centered

--- www/src/pages/index.tsx
+++ www/src/pages/index.tsx
@@ -8,57 +8,65 @@ import SEO from "../components/seo";
 const IndexPage = () => (
   <div
     css={css`
-      margin-top: 128px;
+      /* TODO: make this calc less hacky */
+      height: calc(100vh - 64px);
       display: flex;
-      flex-direction: column;
       justify-content: center;
       align-items: center;
     `}
   >
-    <SEO title="Home" />
     <div
       css={css`
-        margin-bottom: 16px;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
       `}
     >
-      <TerrenceWongPhoto />
-    </div>
-    <h1>terrence wong</h1>
-    <ul
-      css={css`
-        list-style: none;
-        margin: 0;
+      <SEO title="Home" />
+      <div
+        css={css`
+          margin-bottom: 16px;
+        `}
+      >
+        <TerrenceWongPhoto />
+      </div>
+      <h1>terrence wong</h1>
+      <ul
+        css={css`
+          list-style: none;
+          margin: 0;
 
-        li:not(:last-child) {
-          margin-right: 16px;
-        }
-      `}
-    >
-      <li>
-        <a href="https://twitter.com/@terrencito">
-          <span role="img" aria-label="Twitter">
-            🐤
-          </span>{" "}
-          twitter
-        </a>
-      </li>
-      <li>
-        <a href="https://github.com/terrencewwong">
-          <span role="img" aria-label="GitHub">
-            🐙
-          </span>{" "}
-          github
-        </a>
-      </li>
-      <li>
-        <Link to="/blog">
-          <span role="img" aria-label="Blog">
-            ✏️
-          </span>{" "}
-          blog
-        </Link>
-      </li>
-    </ul>
+          li:not(:last-child) {
+            margin-right: 16px;
+          }
+        `}
+      >
+        <li>
+          <a href="https://twitter.com/@terrencito">
+            <span role="img" aria-label="Twitter">
+              🐤
+            </span>{" "}
+            twitter
+          </a>
+        </li>
+        <li>
+          <a href="https://github.com/terrencewwong">
+            <span role="img" aria-label="GitHub">
+              🐙
+            </span>{" "}
+            github
+          </a>
+        </li>
+        <li>
+          <Link to="/blog">
+            <span role="img" aria-label="Blog">
+              ✏️
+            </span>{" "}
+            blog
+          </Link>
+        </li>
+      </ul>
+    </div>
   </div>
 );
 
