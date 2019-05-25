7515a182d1aa6e349ffd9b912ece62a5443e72dd
Terrence Wong <wongterrencew@gmail.com>
2019-05-25 02:43:44 +0200

    fix property name

--- pokemon-shirts/src/components/ArrowBack.js
+++ pokemon-shirts/src/components/ArrowBack.js
@@ -5,7 +5,7 @@ export default function ArrowBack(props) {
     <svg
       stroke="currentColor"
       fill="currentColor"
-      stroke-width="0"
+      strokeWidth="0"
       viewBox="0 0 512 512"
       height="1em"
       width="1em"
