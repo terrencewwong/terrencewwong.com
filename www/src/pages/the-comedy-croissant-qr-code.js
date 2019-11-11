56f01a9774e9b3f15f5d9ba7eec8716307fbcb76
Terrence Wong <wongterrencew@gmail.com>
2019-11-11 23:18:43 +0100

    fix: no vertical scroll

--- www/src/pages/the-comedy-croissant-qr-code.js
+++ www/src/pages/the-comedy-croissant-qr-code.js
@@ -1,10 +1,18 @@
 import React from "react";
 import QRCode from "qrcode.react";
 import SEO from "../components/seo";
+import { css } from "@emotion/core";
 
 export default function TheComedyCroissantQRCode() {
   return (
-    <>
+    <div
+      css={css`
+        display: flex;
+        height: calc(100vh - 64px);
+        justify-content: center;
+        align-items: center;
+      `}
+    >
       <SEO title="The Comedy Croissant QR Code" />
       <QRCode
         value="https://www.facebook.com/pg/thecomedycroissant/events"
@@ -15,6 +23,6 @@ export default function TheComedyCroissantQRCode() {
         includeMargin={false}
         renderAs={"svg"}
       />
-    </>
+    </div>
   );
 }
